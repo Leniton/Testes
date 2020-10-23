0a930b09270a0c4352d182bb5f056d9629373247
Leniton <48127860+Leniton@users.noreply.github.com>
2020-10-23 18:52:52 -0300

    custom input

---
+++ testes/Assets/Custom Input/Custom_Input.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Custom_Input : MonoBehaviour
+{
+    //326
+    [SerializeField] List<string> inputName;
+    [SerializeField] List<KeyCode> inputButton;
+    public Dictionary<string, KeyCode> inputs;
+    public KeyCode jumpkey;
+    public bool checking;
+
+    /*void Start()
+    {
+        print((int)KeyCode.A);
+    }*/
+
+    void Update()
+    {
+        if (checking)
+        {
+            if (Input.anyKeyDown)
+            {
+                checkButton();
+            }
+        }
+    }
+
+    void checkButton()
+    {
+        KeyCode pressed;
+        for (int i = 0; i < 326; i++)
+        {
+            if (Input.GetKeyDown((KeyCode)i)){
+                print((KeyCode)i);
+            }
+        }
+    }
+}
---
+++ testes/Assets/Custom Input/Editor/Input_Window.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using System;
+using System.Data;
+using UnityEditor.EditorTools;
+
+public class Input_Window : EditorWindow
+{
+    public int Nbuttons = 3;
+    List<string> Names = new List<string>();
+
+    List<SearchField> searchFields = new List<SearchField>();
+    List<string> pqs = new List<string>();
+    List<string> txt = new List<string>();
+
+    KeyCode kc = KeyCode.A;
+
+    [MenuItem("Window/Custom input")]
+    public static void Show_Window()
+    {
+        GetWindow<Input_Window>();
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Add"))
+        {
+            Nbuttons++;
+        }
+
+        for (int i = 0; i < Nbuttons; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+
+            if (Names.Count != Nbuttons)
+            {
+                FillLists();
+            }
+
+            Names[i] = EditorGUILayout.TextField(Names[i]);
+
+            txt[i] = searchFields[i].OnToolbarGUI(pqs[i]);
+            if(txt[i] != pqs[i])
+            {
+                pqs[i] = txt[i];
+            }
+
+            if (searchFields[i].HasFocus())
+            {
+
+            }
+
+            if (GUILayout.Button("remove"))
+            {
+                Names.RemoveAt(i);
+
+                searchFields.RemoveAt(i);
+                txt.RemoveAt(i);
+                pqs.RemoveAt(i);
+
+                Nbuttons--;
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        EditorGUILayout.Space();
+
+        /*if (GUILayout.Button("criar"))
+        {
+            Debug.Log("criar");
+        }*/
+    }
+
+    void FillLists()
+    {
+        for (int i = 0; i < Nbuttons; i++)
+        {
+            if(i >= Names.Count)
+            {
+                Names.Add(i.ToString());
+                
+            }
+            if (i >= pqs.Count)
+            {
+                searchFields.Add(new SearchField());
+                txt.Add("");
+                pqs.Add("botão");
+            }
+
+        }
+        //Custom_Input ci;
+    }
+
+    void Sugestions()
+    {
+        //EditorGUILayout.EnumFlagsField(kc);
+        GUI.Label(new Rect(0, 0, 20, 80), "asfgtr");
+    }
+}
