c85df5f79052b99ea06f5a8379aca79768f917f3
Leniton <48127860+Leniton@users.noreply.github.com>
2021-08-10 10:12:53 -0300

    Mirror Networking

---
+++ testes/Assets/Network (Mirror)/PongNetworkManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PongNetworkManager : NetworkManager
+{
+    public List<Transform> Spawns = new List<Transform>();
+
+    public GameObject Ball;
+
+    public List<PongPlayer> playerList = new List<PongPlayer>();
+
+    public override void OnServerAddPlayer(NetworkConnection con)
+    {
+        Transform spawnPoint = Spawns[numPlayers];
+        GameObject player = Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        player.GetComponent<PongPlayer>().texto = FindObjectOfType<Canvas>().transform.GetChild(numPlayers).GetComponent<UnityEngine.UI.Text>();
+        playerList.Add(player.GetComponent<PongPlayer>());
+
+        NetworkServer.AddPlayerForConnection(con, player);
+
+        if (numPlayers == 2)
+        {
+            Ball = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Ball"));
+            Ball.GetComponent<PongBall>().networkManager = this;
+            NetworkServer.Spawn(Ball);
+        }
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        if (Ball != null)
+            NetworkServer.Destroy(Ball);
+
+        playerList.Clear();
+
+        base.OnServerDisconnect(conn);
+    }
+}
---
+++ testes/Assets/Network (Mirror)/PongPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+
+public class PongPlayer : NetworkBehaviour
+{
+    public int pontuação = 0;
+    public Text texto;
+
+    public float speed;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            if (SystemInfo.supportsAccelerometer)
+            {
+                rb.velocity = new Vector2(0, Input.acceleration.y * speed);
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rb.velocity = Vector2.up * speed;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                rb.velocity = Vector2.down * speed;
+            }
+            else
+            {
+                rb.velocity = Vector2.zero;
+            }
+        }
+
+        texto.text = pontuação.ToString(); ;
+    }
+}
