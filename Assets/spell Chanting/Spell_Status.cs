a585b025f1f479f79c5b98b7a017f6492163a79d
Leniton <lenitoncarneiro@gmail.com>
2023-12-21 17:00:14 -0300

    assets

---
+++ Assets/spell Chanting/Spell_Status.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell_Status : MonoBehaviour {
+
+	public float dano;
+	public float velocidade;
+
+	Rigidbody rb;
+
+	void Start () {
+		
+	}
+	
+	public void lançar (){
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = transform.forward * velocidade;
+		Destroy (gameObject, 2);
+	}
+}
---
+++ Assets/spell Chanting/Spell_element.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell_element : MonoBehaviour {
+
+	public Material[] magia;
+	public ParticleSystem[] particulas;
+
+	public void MakeElement(string Element, GameObject localCast){
+		GameObject form;
+		form = localCast.transform.GetChild(0).gameObject;
+		switch (Element) {
+		case "Fogoe:":
+			FireElement (form);
+			break;
+		case"Geloe:":
+			IceElement (form);
+			break;
+		case"Raioe:":
+			RaioElement (form);
+			break;
+		}
+	}
+
+	void FireElement(GameObject form){
+		form.GetComponent<MeshRenderer>().material.color = Color.red;
+	}
+
+	void IceElement(GameObject form){
+		form.GetComponent<MeshRenderer>().material.color = Color.cyan;
+	}
+
+	void RaioElement(GameObject form){
+		form.GetComponent<MeshRenderer>().material.color = Color.yellow;
+	}
+}
