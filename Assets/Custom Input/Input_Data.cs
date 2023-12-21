471af419342990e64c5dab2e4db69dcfc9eb327c
Leniton <lenitoncarneiro@gmail.com>
2023-12-21 16:45:00 -0300

    Removing various tests

--- Assets/Custom Input/Input_Data.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewInputData", menuName = "Input data")]
-public class Input_Data : ScriptableObject
-{
-    public Dictionary<string,KeyCode> inputs = new Dictionary<string, KeyCode>();
-}
