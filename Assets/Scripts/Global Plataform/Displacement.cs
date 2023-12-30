982c1fc51026602f32a8b53a74f4451af2b3d53f
Leniton <lenitoncarneiro@gmail.com>
2023-12-30 09:43:23 -0300

    orientation for future slope uses

--- Assets/Scripts/Global Plataform/Displacement.cs
+++ Assets/Scripts/Global Plataform/Displacement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public abstract class Displacement
 {
     protected PhysicsHandler physicsHandler;
+    [SerializeField] public Vector3 orientation = Vector3.up;
 
     public virtual void Init(PhysicsHandler handler)
     {
