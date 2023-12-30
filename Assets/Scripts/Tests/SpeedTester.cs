5b71c2fdb118654f4dfa61f6278a5bd66ac17da7
Leniton <lenitoncarneiro@gmail.com>
2023-12-30 09:11:25 -0300

    testing acceleration and deceleration

--- Assets/Scripts/Tests/SpeedTester.cs
+++ Assets/Scripts/Tests/SpeedTester.cs
@@ -7,15 +7,46 @@ public class SpeedTester : MonoBehaviour
     PhysicsHandler physicsHandler;
     float prevCheckpointTime = 0;
 
+    [SerializeField] float speedTrigger;
+
     private void Awake()
     {
         physicsHandler = GetComponent<PhysicsHandler>();
         physicsHandler.TriggerEnter += CheckSpeed;
+        StartCoroutine(Test(speedTrigger));
     }
 
     private void CheckSpeed(ColliderData data)
     {
         Debug.Log($"hit at {physicsHandler.GetVelocity().x} speed on {Time.time - prevCheckpointTime} seconds");
         prevCheckpointTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StopAllCoroutines();
+            StartCoroutine(Test(0));
+        }
+        if(Input.GetKeyUp(KeyCode.A))
+        {
+            StopAllCoroutines();
+            StartCoroutine(Test(speedTrigger));
+        }
+    }
+
+    IEnumerator Test(float goal)
+    {
+        float timing = 0;
+        float currentSpeed = physicsHandler.GetVelocity().x;
+        do
+        {
+            yield return new WaitForFixedUpdate();
+            timing += Time.fixedDeltaTime;
+            currentSpeed = physicsHandler.GetVelocity().x;
+
+        } while(currentSpeed != goal);
+        Debug.Log($"reached speed in {timing} seconds");
     }
 }
