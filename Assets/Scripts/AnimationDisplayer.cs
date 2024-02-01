9e41d858c95fbd7fef6fe97de21cb65afb1462ff
Leniton <lenitoncarneiro@gmail.com>
2024-02-01 18:04:27 -0300

    testing use of animation curves

--- Assets/Scripts/AnimationDisplayer.cs
+++ Assets/Scripts/AnimationDisplayer.cs
@@ -8,15 +8,15 @@ public class AnimationDisplayer : MonoBehaviour
     [SerializeField] Transform movingObject;
     [SerializeField] Procedural_Animator animator;
     [Header("Time values")]
-    [SerializeField] float duration,timeModifier = 1,timeRange = 1;
+    [SerializeField] float timeModifier = 1;
+    [SerializeField] AnimationCurve curve;
     float currentTime = 0;
 
     Vector3 initialPos;
 
     void Start()
     {
-        initialPos = animator.initialPos();
-
+        initialPos = movingObject.transform.position;
         bit b = new bit(true);
         //b ^= 1;
         Debug.Log(b);
@@ -24,11 +24,23 @@ public class AnimationDisplayer : MonoBehaviour
 
     void Update()
     {
-        return;
-        if (currentTime <= duration)
+        if (currentTime <= 1)
         {
             currentTime += Time.deltaTime * timeModifier;
-            movingObject.position = initialPos + (Vector3.right * animator.TimeToReact(currentTime)*timeRange) +
+            Vector3 value = Vector3.zero;
+            value.x = (-initialPos.x*2) * curve.Evaluate(currentTime);
+            movingObject.position = initialPos + value;
+        }
+        else
+        {
+            currentTime = 0;
+        }
+
+        return;
+        if (currentTime <= 1)
+        {
+            currentTime += Time.deltaTime * timeModifier;
+            movingObject.position = initialPos + (Vector3.right * animator.TimeToReact(currentTime)) +
                 Vector3.up * animator.FullAnimation(currentTime);
         }
         else
