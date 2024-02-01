2508f1e68b41dec187c3df4c019cf42a4d464321
Leniton <lenitoncarneiro@gmail.com>
2024-02-01 17:32:30 -0300

    reorganizing

--- Assets/Scripts/AnimationDisplayer.cs
+++ Assets/Scripts/AnimationDisplayer.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationCurve : MonoBehaviour
+public class AnimationDisplayer : MonoBehaviour
 {
     [SerializeField] Transform movingObject;
     [SerializeField] Procedural_Animator animator;
