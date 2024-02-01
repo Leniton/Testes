42d166c985bbab117502ba01b40998c0b0cbdcc3
Leniton <lenitoncarneiro@gmail.com>
2024-02-01 18:42:51 -0300

    clean up

--- Assets/Scripts/AnimationDisplayer.cs
+++ Assets/Scripts/AnimationDisplayer.cs
@@ -17,9 +17,6 @@ public class AnimationDisplayer : MonoBehaviour
     void Start()
     {
         initialPos = movingObject.transform.position;
-        bit b = new bit(true);
-        //b ^= 1;
-        Debug.Log(b);
     }
 
     void Update()
@@ -35,85 +32,5 @@ public class AnimationDisplayer : MonoBehaviour
         {
             currentTime = 0;
         }
-
-        return;
-        if (currentTime <= 1)
-        {
-            currentTime += Time.deltaTime * timeModifier;
-            movingObject.position = initialPos + (Vector3.right * animator.TimeToReact(currentTime)) +
-                Vector3.up * animator.FullAnimation(currentTime);
-        }
-        else
-        {
-            currentTime = 0;
-        }
     }
-}
-
-public struct InterpolationCurve
-{
-    //values used in movement
-    float currentValue;
-    float currentSpeed;
-    float currentAceleration;
-
-    //values used for changing the linear movement
-    float _acelerationRate;
-
-    public InterpolationCurve(float startValue, float endValue,float acelerationRate=0)
-    {
-        _acelerationRate = acelerationRate;
-
-        currentValue = startValue;
-        currentSpeed = endValue - startValue;
-        currentAceleration = currentSpeed * _acelerationRate;
-    }
-
-    public float Step(float deltaTime)
-    {
-        currentSpeed += currentAceleration * deltaTime;
-        currentValue += currentSpeed * deltaTime;
-        return currentValue;
-    }
-}
-
-public struct bit
-{
-    private byte value;
-    public bit(bool on = false) => value = (byte)(on ? 1 : 0);
-    private static byte ToBit(byte by) => (byte)(by > 0 ? 1 : 0);
-
-    public static implicit operator byte(bit bit) => bit.value;
-    public static implicit operator int(bit bit) => bit.value;
-    public static explicit operator bit(int n) => new bit(n > 0);
-
-    public static bit operator +(bit b, byte by)
-    {
-        b.value = ToBit((byte)(b.value + by));
-        return b;
-    }
-    public static bit operator -(bit b, byte by)
-    {
-        b.value = ToBit((byte)(b.value - by));
-        return b;
-    }
-    public static bit operator |(bit b, byte by)
-    {
-        b.value = (byte)(ToBit(by) | b.value);
-        return b;
-    }
-    public static bit operator &(bit b, byte by)
-    {
-        b.value = (byte)(ToBit(by) & b.value);
-        return b;
-    }
-    public static bit operator ^(bit b, byte by)
-    {
-        b.value = (byte)(ToBit(by) ^ b.value);
-        return b;
-    }
-    public static bit operator ~(bit b) => b ^ 1;
-    public static bit operator !(bit b) => ~b;
-
-    public override string ToString() => value.ToString();
 }
--- Assets/Scripts/Procedural_Animator.cs
+++ Assets/Scripts/Procedural_Animator.cs
@@ -1,9 +1,6 @@
-using Newtonsoft.Json.Linq;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.AI;
 
+[ExecuteAlways]
 public class Procedural_Animator : MonoBehaviour
 {
     [SerializeField] Transform sampleParent;
@@ -17,21 +14,9 @@ public class Procedural_Animator : MonoBehaviour
     [SerializeField] Transform TTR_SampleParent;
     [SerializeField, Range(0, 1)] float timeToReact;
     [Range(.1f, .9f)] public float timeBalance;
-    [Space]
-    [Header("InitialResponse")]
-    [SerializeField] Transform IR_SampleParent;
-    [SerializeField, Range(-1, 1)] float initialResponse;
-    [Space]
-    [Header("DampEffect")]
-    [SerializeField] Transform DE_SampleParent;
-    [SerializeField, Range(0, 2)] float dampEffect;
-    [Space]
-    [Header("InitialResponse")]
-    [SerializeField] Transform FinalAnimationParent;
     float timeStep => (1f / (sampleParent.childCount / 3f));
     float changeTime => timeStep * changeID;
-    [Space]
-    [SerializeField] float k1=1, k2=1, k3 = 1;
+
     void Update()
     {
         //startLine
@@ -54,10 +39,6 @@ public class Procedural_Animator : MonoBehaviour
                 Vector3.up * ((rect.height) * step * (i - sampleDivision));
         }
 
-        //!!! all values are proportional to inputChange
-
-        //time to resepond: lerp rate for initial response center
-        //line code for timeTo respond
         step = 1f / (TTR_SampleParent.childCount/2);
 
         for (int i = 0; i < TTR_SampleParent.childCount; i++)
@@ -65,33 +46,6 @@ public class Procedural_Animator : MonoBehaviour
             float time = step * i;
             //Debug.Log($"input: {time} | output: {DetermineAlphaValue(time, 0, timeBalance, 1 - timeBalance)}");
             TTR_SampleParent.GetChild(i).position = rect.min + (Vector2.right * rect.max * time) + Vector2.up * (GetY(time));
-        }
-
-        return;
-        //initial Response: amplitude of sine wave based on time to respond value
-        //line code for initial response
-        step = 1f / IR_SampleParent.childCount;
-        for (int i = 0; i < IR_SampleParent.childCount; i++)
-        {
-            IR_SampleParent.GetChild(i).position = rect.min + Vector2.right * (rect.width * TimeToReact(step * i)) +
-                Vector2.up * (InitialResponse(initialResponse, step * i));
-        }
-        //damp value: drag; value that reduces the initial response amplitude over time. proportional to initial response
-        //line code for damp value(?)
-        step = (1f / DE_SampleParent.childCount) * 2;
-        for (int i = 0; i < DE_SampleParent.childCount; i++)
-        {
-
-            DE_SampleParent.GetChild(i).position = rect.min + Vector2.right * (rect.width * TimeToReact(step * i)) +
-                Vector2.up * (InitialResponse(DampEffect(initialResponse, step * i, dampEffect), step * i));
-        }
-        //adding modifiers in a single sample
-        step = (1f / FinalAnimationParent.childCount) * 4;
-        for (int i = 0; i < FinalAnimationParent.childCount; i++)
-        {
-            float time = step * i;
-            FinalAnimationParent.GetChild(i).position = rect.min + Vector2.right * (rect.width * TimeToReact(time)) +
-                Vector2.up * (FullAnimation(time));
         }
     }
 
@@ -106,41 +60,8 @@ public class Procedural_Animator : MonoBehaviour
         time = time-1 == 0 ? .01f : time - 1;
         //Debug.Log(time);
         float y = inputChange * Mathf.Clamp01(DetermineAlphaValue(time, changeTime, 99999, timeStep));
-        float derivative1 = 1 / time;
-        float derivative2 = derivative1 / time;
-        y -= k1 * derivative1;
-        y -= k2 * derivative2;
-        y += k3 * derivative1;
 
         return y;
-    }
-
-    //velocity = displacement/time
-    //acceleration = velocity/time | (displacement/time)/time
-
-    public float TimeToReact(float time)
-    {
-        float currentTime = time;
-        return currentTime * timeToReact;
-    }
-
-    float InitialResponse(float amp, float time, float frequency = .5f)
-    {
-        return inputChange * amp * Mathf.Sin(2 * Mathf.PI * frequency/*frequency*/ * time);
-    }
-
-    float DampEffect(float delta,float time,float damp = 1)
-    {
-        float max = Mathf.Max(delta, 0);
-        float min = Mathf.Min(delta, 0);
-        return Mathf.Clamp(delta * (1 - (damp) * time), min, max);
-    }
-
-    public float FullAnimation(float time)
-    {
-        float delta = inputChange * (Mathf.Clamp01(time));
-        delta += InitialResponse(DampEffect(initialResponse, time, dampEffect), time);
-        return delta;
     }
 
     public Vector2 initialPos()
@@ -166,4 +87,73 @@ public class Procedural_Animator : MonoBehaviour
     }
 
 
+}
+
+
+public struct InterpolationCurve
+{
+    //values used in movement
+    float currentValue;
+    float currentSpeed;
+    float currentAceleration;
+
+    //values used for changing the linear movement
+    float _acelerationRate;
+
+    public InterpolationCurve(float startValue, float endValue, float acelerationRate = 0)
+    {
+        _acelerationRate = acelerationRate;
+
+        currentValue = startValue;
+        currentSpeed = endValue - startValue;
+        currentAceleration = currentSpeed * _acelerationRate;
+    }
+
+    public float Step(float deltaTime)
+    {
+        currentSpeed += currentAceleration * deltaTime;
+        currentValue += currentSpeed * deltaTime;
+        return currentValue;
+    }
+}
+
+public struct bit
+{
+    private byte value;
+    public bit(bool on = false) => value = (byte)(on ? 1 : 0);
+    private static byte ToBit(byte by) => (byte)(by > 0 ? 1 : 0);
+
+    public static implicit operator byte(bit bit) => bit.value;
+    public static implicit operator int(bit bit) => bit.value;
+    public static explicit operator bit(int n) => new bit(n > 0);
+
+    public static bit operator +(bit b, byte by)
+    {
+        b.value = ToBit((byte)(b.value + by));
+        return b;
+    }
+    public static bit operator -(bit b, byte by)
+    {
+        b.value = ToBit((byte)(b.value - by));
+        return b;
+    }
+    public static bit operator |(bit b, byte by)
+    {
+        b.value = (byte)(ToBit(by) | b.value);
+        return b;
+    }
+    public static bit operator &(bit b, byte by)
+    {
+        b.value = (byte)(ToBit(by) & b.value);
+        return b;
+    }
+    public static bit operator ^(bit b, byte by)
+    {
+        b.value = (byte)(ToBit(by) ^ b.value);
+        return b;
+    }
+    public static bit operator ~(bit b) => b ^ 1;
+    public static bit operator !(bit b) => ~b;
+
+    public override string ToString() => value.ToString();
 }
