59979095748de8494425bcb2ebd699011e6f3d56
Leniton <lenitoncarneiro@gmail.com>
2024-04-30 09:01:48 -0300

    fully working

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -6,9 +6,9 @@ public class TestScript : MonoBehaviour
 {
     [SerializeField] private Texture2D tex;
     private RawImage image;
-    [SerializeField,Range(0,11)] private int index;
+    [SerializeField] private int index, min;
     [SerializeField] private Sprite[] images;
-    [SerializeField] private TexData texData;
+    [SerializeField] int numberSpacing;
     private bool isNumber => index < 10;
 
     void Awake()
@@ -47,28 +47,60 @@ public class TestScript : MonoBehaviour
 
     private void MountTexture(int maxValue = 10)
     {
-        Texture2D finalTex = new Texture2D((int)images[11].rect.width, maxValue * (int)images[11].rect.height);
+        Texture2D finalTex = new Texture2D((int)images[11].rect.width, (maxValue-min) * (int)images[11].rect.height);
         finalTex.filterMode = FilterMode.Point;
         Texture2D bg = TextureHelper.GetTexPart(tex, images[11].rect);
 
-        for (int i = 0; i < maxValue; i++)
+        for (int i = min; i < maxValue; i++)
         {
-            TextureHelper.MergeTexture(finalTex, bg, new(0, bg.height * i));
+            TextureHelper.MergeTexture(finalTex, bg, new(0, bg.height * (i-min)));
         }
 
         //for now clamped at 9
-        int values = Mathf.Min(maxValue, 9);
-        for (int i = 0; i < values; i++)
+        int values = maxValue;//Mathf.Min(maxValue, 9);
+        int width = (int)images[0].rect.width;
+        int height = (int)images[0].rect.height;
+        for (int i = min; i < values; i++)
         {
-            Texture2D number = TextureHelper.GetTexPart(tex, images[i + 1].rect);
-            Vector2Int offset = new Vector2Int(0, bg.height * i);// new((bg.width / 2), (bg.height / 2));
-            offset.x = (bg.width / 2) - (number.width/2);
-            offset.y += (bg.height / 2) - (number.height/2);
+            Vector2Int offset = new Vector2Int(0, bg.height * (i-min));
+            offset.x = (bg.width / 2);// - (number.width/2);
+            offset.y += (bg.height / 2) - (height/2);
 
-            TextureHelper.MergeTexture(finalTex, number, offset);
+            
+            //offset for multiple numbers
+            int numbers = 1;
+            int ten = 10;
+            //verify up to 100000
+            for (int o = 0; o < 5; o++)
+            {
+                if ((i+1) / ten > 0)
+                {
+                    numbers++;
+                }
+                ten *= 10;
+            }
+            //add spacing
+            int fullSpace = width * numbers + (numberSpacing * (numbers - 1));
+            int initialOffset = fullSpace / 2;
+            int spacing = width + numberSpacing;
+            offset.x -= initialOffset;
+            for (int s = numbers-1; s >= 0; s--)
+            {
+                Texture2D number = TextureHelper.GetTexPart(tex, images[GetDigit(i + 1, s)].rect);
+                TextureHelper.MergeTexture(finalTex, number, offset);
+                offset.x += spacing;
+            }
+
         }
 
         RenderTexture(finalTex);
+    }
+
+    private int GetDigit(int number, int digitID = 0)
+    {
+        int value = number;
+        if (digitID > 0) value = Mathf.FloorToInt(number / Mathf.Pow(10, digitID));
+        return value % 10;
     }
 }
 
