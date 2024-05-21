d1c39cd221fb6585902f020c61e8592d7acffdf9
Leniton <lenitoncarneiro@gmail.com>
2024-05-21 19:19:08 -0300

    more tests

--- Assets/Resources/Editor/SerializedObjects/SerializedTestInterface.cs
+++ Assets/Resources/Editor/SerializedObjects/SerializedTestInterface.cs
@@ -1,16 +1,18 @@
-using System;
+using UnityEngine;
 using SerializableMethods;
-using UnityEditor.UIElements;
+using System;
 using UnityEngine.UIElements;
+using UnityEditor.UIElements;
 using Object = UnityEngine.Object;
 
-public class SerializedTestClass : ISerializedObject
+public class SerializedTestInterface : ISerializedObject
 {
-    public Type[] usedTypes => new [] { typeof(TestGenericClass) };
+    public Type[] usedTypes => new[] { typeof(ITestGenericInterface) };
+
     public VisualElement GetElement(string label, object value, Type type, Action<object> onValueChanged)
     {
         ObjectField field = new ObjectField(label);
-        field.objectType = type;
+        field.objectType = typeof(ITestGenericInterface);
         field.value = (Object)value;
         field.RegisterCallback<ChangeEvent<Object>>((evt) => onValueChanged?.Invoke(evt.newValue));
         return field;
