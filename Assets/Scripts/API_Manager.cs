eb9c0ac623c0e489d244d61a62aec47ad6f26283
Leniton <lenitoncarneiro@gmail.com>
2024-06-13 18:06:27 -0300

    forgoten rest

--- Assets/Scripts/API_Manager.cs
+++ Assets/Scripts/API_Manager.cs
@@ -1,14 +1,35 @@
-
 using UnityEngine;
+using UnityEngine.UI;
 
 public class API_Manager : MonoBehaviour
 {
     [SerializeField] private RectTransform MainParent;
+    [SerializeField] CreationMenu CreationMenu;
+    [SerializeField] Button copyButton;
+    private MonoAbstraction currentCreation;
     
     private void Awake()
     {
-        //Debug.Log(DiceSideDatabase.sides[1].name);
-        MonoAbstraction item = WindowGenerator.Item_Keyword();
-        item.transform.SetParent(MainParent, false);
+        CreationMenu.onCreated += CreateAbstraction;
+        PickCreation();
+        copyButton.onClick.AddListener(CopyCode);
+    }
+
+    public void PickCreation()
+    {
+        if (currentCreation != null) Destroy(currentCreation.gameObject);
+        CreationMenu.OpenOptions();
+    }
+
+    public void CreateAbstraction(MonoAbstraction abstraction)
+    {
+        currentCreation = abstraction;
+        abstraction.transform.SetParent(MainParent, false);
+        abstraction.transform.SetAsFirstSibling();
+    }
+
+    public void CopyCode()
+    {
+        GUIUtility.systemCopyBuffer = currentCreation.GetCode(null);
     }
 }
