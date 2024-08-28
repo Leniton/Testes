e89bdc8c2bcd268b0637c4f503f1d09daa7d9303
Leniton <lenitoncarneiro@gmail.com>
2024-08-28 14:34:05 -0300

    base

---
+++ Assets/Scripts/PlayableTests.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+[RequireComponent(typeof(Animator))]
+public class PlayableTests : MonoBehaviour
+{
+
+    public AnimatorControllerPlayable a;
+    public Animator b;
+
+    void Start()
+    {
+        
+    }
+}
