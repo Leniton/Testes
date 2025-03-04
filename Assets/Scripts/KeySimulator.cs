04b6f0146c13fad52d3e7b6cf768df53046be63f
Leniton <lenitoncarneiro@gmail.com>
2025-03-04 10:33:30 -0300

    some tests

--- Assets/Scripts/KeySimulator.cs
+++ Assets/Scripts/KeySimulator.cs
@@ -14,7 +14,7 @@ public class KeySimulator : MonoBehaviour
     public static void SetUP()
     {
         Process[] processes = Process.GetProcessesByName("DMT");
-        Debug.Log(processes.Length);
+        //Debug.Log(processes.Length);
     }
 
     private void Awake()
@@ -27,7 +27,8 @@ public class KeySimulator : MonoBehaviour
         while (simulate)
         {
             yield return new WaitForSeconds(1);
-            sim.Mouse.MoveMouseBy(-5, 0);
+            //sim.Mouse.MoveMouseBy(-5, 0);
+            sim.Keyboard.KeyPress(VirtualKeyCode.VK_K);
         }
     }
 
@@ -35,8 +36,8 @@ public class KeySimulator : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //simulate = !simulate;
-            //if(simulate) StartCoroutine(ConstantCall());
+            simulate = !simulate;
+            if(simulate) StartCoroutine(ConstantCall());
             //sim.Keyboard.KeyPress(VirtualKeyCode.VK_K);
             /*sim.Keyboard.KeyDown(VirtualKeyCode.LWIN)
                 .KeyDown(VirtualKeyCode.LMENU)
@@ -44,20 +45,22 @@ public class KeySimulator : MonoBehaviour
                 .KeyUp(VirtualKeyCode.LWIN)
                 .KeyUp(VirtualKeyCode.LMENU);*/
             //sim.Mouse.MoveMouseBy(2, 2);\
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            //startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            /*ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = "\"C:\\Users\\leniton.carneiro\\AppData\\Local\\Programs\\Opera GX\\launcher.exe\"";
             startInfo.WorkingDirectory = string.Empty;
             startInfo.UseShellExecute = false;
             startInfo.Arguments = " --side-profile-name=31343533325F31313537303035323933 --side-profile-minimal --side-profile-clear-on-exit --side-profile-muted --side-profile-no-gx-sounds --with-feature:side-profiles --private";
             Process process = new Process();
             process.StartInfo = startInfo;
-            process.Start();
+            process.Start();*/
         }
 
         //if (Input.GetKeyDown(KeyCode.K)) Debug.Log("key down");
-        if (sim.InputDeviceState.IsKeyDown(VirtualKeyCode.VK_K)) Debug.Log("key down");
-        if (Input.GetKey(KeyCode.K)) Debug.Log("key");
-        if (Input.GetKeyUp(KeyCode.K)) Debug.Log("key up");
+        //if (Input.GetKeyDown(KeyCode.K)) sim.Keyboard.KeyPress(VirtualKeyCode.VK_M);
+        //if (sim.InputDeviceState.IsKeyDown(VirtualKeyCode.VK_M)) Debug.Log("key? down");
+        //if (Input.GetKeyDown(KeyCode.M)) Debug.Log("key down");
+        //if (Input.GetKey(KeyCode.M)) Debug.Log("key");
+        //if (Input.GetKeyUp(KeyCode.M)) Debug.Log("key up");
     }
 }
