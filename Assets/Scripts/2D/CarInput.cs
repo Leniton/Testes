406dd13c8a1fd631143dbc900609018ff06c3c4e
Leniton <lenitoncarneiro@gmail.com>
2026-01-04 15:24:35 -0300

    feat: aim rotation

--- Assets/Scripts/2D/CarInput.cs
+++ Assets/Scripts/2D/CarInput.cs
@@ -12,8 +12,21 @@ public class CarInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 input = Vector2.zero;
+        if (Input.GetKey(KeyCode.Space)) input.y = 1;
+        else input.y = 0;
+        //input.x = Input.GetAxis("Horizontal");
+        //if(Input.GetMouseButtonDown(0)) CalculateDirection();
+        if(input.y != 0) movement.targetDirection = input.y != 0 ? CalculateDirection() : null;
         movement.input = input;
+    }
+
+    private Vector3 CalculateDirection()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        var direction = (mousePosition - transform.position).normalized;
+        return direction;
     }
 
     private void Reset()
--- Assets/Scripts/2D/CarMovement.cs
+++ Assets/Scripts/2D/CarMovement.cs
@@ -9,12 +9,13 @@ public class CarMovement : MonoBehaviour
     private PhysicsHandler physicsHandler;
 
     public Vector2 input;
-    [SerializeField]private float grip = .5f;
+    public Vector2? targetDirection;
+    [SerializeField, Range(0.01f, 1)] private float maxTurnSpeed = .1f;
+    [SerializeField] private float grip = .5f;
     [SerializeField] private Movement movement;
     [SerializeField] private Movement turnMovement;
 
     private Vector2 lastInput;
-    private float currentSpeed;
     
     private void Awake()
     {
@@ -24,26 +25,34 @@ public class CarMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         float speed = movement.Move(input * Vector2.up).y;
         if (input.y < 0 || (input.y == 0 && lastInput.y < 0)) speed *= .4f;
-        transform.Rotate(Vector3.forward, turnMovement.Move(input * Vector2.left).x);
+        if (!targetDirection.HasValue)
+            transform.Rotate(Vector3.forward, turnMovement.Move(input * Vector2.left).x);
+        else RotateToAngle();
         var finalVelocity = HandleVelocity(speed);
-        currentSpeed = speed;
         physicsHandler.Velocity = finalVelocity;
-        Debug.DrawRay(transform.position, finalVelocity, Color.red);
+        //Debug.DrawRay(transform.position, finalVelocity, Color.red);
         if (input != Vector2.zero) lastInput = input;
     }
 
-    Vector3 HandleVelocity(float speed)
+    private Vector3 HandleVelocity(float speed)
     {
         Vector3 movement = speed * transform.up;
         Vector3 currentVelocity = physicsHandler.Velocity;
         var dotProduct = Mathf.Abs(Vector3.Dot(transform.up, currentVelocity.normalized));
         if (Mathf.Approximately(dotProduct, 1)) return movement;
         movement = Vector3.Lerp(currentVelocity, movement, grip);
-        Debug.DrawRay(transform.position, currentVelocity, Color.cyan);
+        //Debug.DrawRay(transform.position, currentVelocity, Color.cyan);
         return movement;
+    }
+
+    private void RotateToAngle()
+    {
+        if(!targetDirection.HasValue) return;
+        transform.rotation = Quaternion.Lerp(transform.rotation,
+            Quaternion.LookRotation(Vector3.forward, targetDirection.Value), maxTurnSpeed);
     }
 }
