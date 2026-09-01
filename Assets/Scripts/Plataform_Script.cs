be2332c90cc9bf2a128700350ddd6e018eaedd7b
Leniton <leniton.carneiro@gdstecnologia.com.br>
2026-09-01 12:24:31 -0300

    draft: simple plataforming

---
+++ Assets/Scripts/Plataform_Script.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+using PhysicsHelper;
+
+[RequireComponent(typeof(PhysicsHandler))]
+public class Plataform_Script : MonoBehaviour
+{
+#if UNITY_EDITOR
+    public bool testMode = true;
+#endif
+    public float levelOfControl = 1;//change to float level of control?(0 no control, 1 full control)
+    public bool useGravity = true;
+    public enum State { idle, walking, jumping }
+    [SerializeField] public State state = new State();
+
+    public Vector3 input = Vector3.zero;
+    //Movement
+    [Header("Walk"), SerializeField] private Movement movement;
+
+    [Header("Jump"), SerializeField] private float controlJumpThreshold;
+    [SerializeField] private Jump _jump;
+
+    private Jump jumpOverride;
+    public Jump Jump
+    {
+        get { return jumpOverride ?? _jump; }
+        set 
+        {
+            value.Initialize(physicsHandler);
+            value.CopyFloorData(Jump);
+            jumpOverride = value; 
+        }
+    }
+
+    private Movement movementOverride;
+    public Movement Movement
+    {
+        get { return movementOverride ?? movement; }
+        set { movementOverride = value; }
+    }
+
+    public PhysicsHandler physicsHandler {  get; private set; }
+
+    //for calculating movement while in moving plataforms
+    private PhysicsHandler physicsSurface;
+    private bool jumped = false;
+
+    void Awake()
+    {
+        physicsHandler = GetComponent<PhysicsHandler>();
+        Jump.Initialize(physicsHandler);
+        Jump.OnLand += OnLanding;
+        movement.Initialize(physicsHandler);
+        physicsHandler.CollisionExit += OnLeavingPlataform;
+    }
+
+    void FixedUpdate()
+    {
+#if UNITY_EDITOR
+        if (testMode)
+        {
+            Jump.CalculateParameters();
+            movement.CalculateParameters();
+        }
+#endif
+
+        Vector3 xInput = Movement.AdjustToNormal(input * Vector2.right, Jump.floorNormal);
+        Vector3 inputVelocity = physicsHandler.Velocity;
+        inputVelocity = (inputVelocity * (1 - levelOfControl)) + (Movement.Move(xInput) * levelOfControl);
+        inputVelocity.y = physicsHandler.Velocity.y;
+
+        if (levelOfControl >= controlJumpThreshold)
+        {
+            if (input.y > 0)
+            {
+                if (Jump.onGround)
+                {
+                    jumped = true;
+                    inputVelocity.y = Jump.JumpValue();
+                    input.y = 0;
+                }
+            }
+        }
+
+        if (useGravity) inputVelocity.y -= Jump.GravityForce();
+
+        Vector3 finalVelocity = inputVelocity;
+        if (!jumped && Jump.onGround && physicsSurface)
+        {
+            finalVelocity.y = 0;
+            Vector3 surfaceVelocity = physicsSurface.Velocity;
+            finalVelocity += surfaceVelocity;
+        }
+
+        physicsHandler.Velocity = finalVelocity;
+
+        //Update state
+        if (!Jump.onGround)
+        {
+            input.y = 0;
+            state = State.jumping;
+        }
+        else
+        {
+            Vector3 moving = inputVelocity - Vector3.Scale(inputVelocity, _jump.orientation);
+            if (moving == Vector3.zero)
+                state = State.idle;
+            else
+                state = State.walking;
+        }
+    }
+
+    private void OnLanding(CollisionData data)
+    {
+        jumped = false;
+        physicsSurface = data.gameObject.GetComponent<PhysicsHandler>();
+    }
+    private void OnLeavingPlataform(CollisionData data)
+    {
+        if (Jump.onGround) return;
+        physicsSurface = null;
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,23 @@
+using InputSystemHelper;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Input = InputSystemHelper.Input;
+using Logger = LenixSO.Logger.Logger;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private Plataform_Script plataform;
+    
+    private void Awake()
+    {
+        plataform ??= GetComponent<Plataform_Script>();
+        if (plataform == null) return;
+        Input.Map("Player").Action("Move").performed += OnMove;
+        Input.Map("Player").Action("Move").canceled += OnMove;
+    }
+    private void OnMove(InputAction.CallbackContext obj)
+    {
+        var data = obj.ReadValue<Vector2>();
+        plataform.input = data;
+    }
+}
