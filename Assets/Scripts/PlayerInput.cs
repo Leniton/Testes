d219c425aa06b231ef21dc3c89ad68292fced0e4
Leniton <leniton.carneiro@gdstecnologia.com.br>
2026-09-02 17:58:18 -0300

    draft: auto direction pick

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -29,7 +29,6 @@ public class PlayerInput : MonoBehaviour
                 {
                     Time.timeScale = .2f;
                     plataform.input = Vector2.zero;
-                    appliedForce = plataform.physicsHandler.ApplyForce(Vector3.zero, 0);
                     plataform.useGravity = false;
                     plataform.levelOfControl = 0;
                 })
@@ -80,25 +79,59 @@ public class PlayerInput : MonoBehaviour
 
     private void EnableCollision(Collider2D collider)
     {
-        collider.isTrigger = true;
+        // collider.isTrigger = true;
         collider.enabled = true;
         List<Collider2D> colliders = new List<Collider2D>();
         var count = Physics2D.OverlapCollider(collider, colliders);
+        Collider2D other = null;
         // Logger.Log(count);
         bool insideCollider = false;
         for (int i = 0; i < count; i++)
         {
             if (colliders[i].gameObject.layer != collider.gameObject.layer) continue;
             insideCollider = true;
+            other = colliders[i];
             break;
         }
         collider.isTrigger = insideCollider;
         if (!insideCollider) return;
         // return;
         //slowdown + launch
+        var exitDirection = Vector3.right;
+        float distance = GetDistance(new Vector3(collider.bounds.min.x, collider.bounds.center.y, 0), Vector3.right);//left
+        GetSmallerDistance(new Vector3(collider.bounds.center.x, collider.bounds.max.y, 0), Vector3.down);//top
+        GetSmallerDistance(new Vector3(collider.bounds.max.x, collider.bounds.center.y, 0), Vector3.left);//right
+        GetSmallerDistance(new Vector3(collider.bounds.center.x, collider.bounds.min.y, 0), Vector3.up);//bot
         plataform.physicsHandler.TriggerExit += OnLeaveCollider;
+        Debug.Log(exitDirection);
+        appliedForce = plataform.physicsHandler.ApplyForce(exitDirection, 0);
         moveSequence.Begin();
         return;
+
+        void GetSmallerDistance(Vector3 origin, Vector3 direction)
+        {
+            float newDistance = GetDistance(origin, direction);
+            // Debug.Log($"{direction} : {newDistance} - {distance}");
+            if (newDistance >= distance) return;
+            distance = newDistance;
+            exitDirection = direction;
+        }
+
+        float GetDistance(Vector3 origin, Vector3 direction)
+        {
+            var results = new List<RaycastHit2D>();
+            var hits = Physics2D.Raycast(origin, direction*0.8f, new ContactFilter2D(), results);
+            // Debug.Log(hits);
+            for (int i = 0; i < hits; i++)
+            {
+                // Debug.Log(results[i].collider.gameObject.name);
+                if (results[i].collider != other) continue;
+                Debug.Log($"{direction} | {results[i].distance} | {results[i].normal}");
+                Debug.DrawRay(origin, direction, Color.red, 1);
+                return results[i].distance;
+            }
+            return float.MaxValue;
+        }
 
         void OnLeaveCollider(ColliderData c)
         {
