04c293f1678db7de7ba18e9afe0ecbbec8a83624
Leniton <lenitoncarneiro@gmail.com>
2026-09-07 11:17:53 -0300

    test: custom visualelement

---
+++ Assets/Scripts/UI/Components/TestCustomVisualElement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine.UIElements;
+
+namespace UI.Components
+{
+    [UxmlElement]
+    public partial class TestCustomVisualElement : VisualElement
+    {
+        [UxmlAttribute] public bool test { get; set; }
+
+        [UxmlCreateInstanceMethod]
+        public static TestCustomVisualElement Create()
+        {
+            TestCustomVisualElement element = new();
+            element.test = true;
+            return element;
+        }
+
+        private void OrganizeElements()
+        {
+            
+        }
+    }
+}
